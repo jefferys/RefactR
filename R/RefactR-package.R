e002094254803263f62f9dc84a2de2f6338b4901
Stuart R. Jefferys <srjefferys@gmail.com>
2016-12-09 17:47:34 -0500

    DEV: Initial commit.

---
+++ R/RefactR-package.R
@@ -0,0 +1,8 @@
+#' RefactR - Refactor R code and tests
+#'
+#' Provides tools to automatically refactor R code, adding or extracting
+#' functions, arguments, tests, documentation, etc.
+#'
+#' @name RefactR
+#' @docType package
+NULL
